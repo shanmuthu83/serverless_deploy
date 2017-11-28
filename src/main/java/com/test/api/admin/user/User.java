f4f10203198a9741bec7723aec0c3fc43130c30d
Shanmugam <smuthanantham@ghx.com>
2017-11-28 19:23:17 +0530

    init

--- src/main/java/com/test/api/admin/user/User.java
+++ src/main/java/com/test/api/admin/user/User.java
@@ -9,26 +9,15 @@ import java.util.Map;
 import org.codehaus.jackson.map.ObjectMapper;
 
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
-import com.test.api.admin.user.req.data.UserRequest;
 
 public class User implements RequestStreamHandler {
 
 	@Override
 	public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
-		LambdaLogger logger = context.getLogger();
+
 		ObjectMapper mapper = new ObjectMapper();
-
-		UserRequest request = mapper.readValue(input, UserRequest.class);
-
-		Object data = null;
-
-		if (request.getField().equalsIgnoreCase("eid")) {
-			data = getAllUsers();
-		} else if (request.getField().equalsIgnoreCase("all")) {
-			data = getAllUsers();
-		}
+		Map<String, Object> data = getAllUsers();
 
 		mapper.writeValue(output, data);
 	}
